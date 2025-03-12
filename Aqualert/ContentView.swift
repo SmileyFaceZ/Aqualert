d3894269f10b562a411d9b119a96ce6e3d443cd9
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2025-03-12 17:08:53 +0700

    Remove unuser line code

--- Aqualert/ContentView.swift
+++ Aqualert/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Aqualert
-//
-//  Created by Kantaphat Phaphui on 12/3/2568 BE.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -13,15 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-<<<<<<< HEAD
             Text("Test Commit")
-=======
-<<<<<<< HEAD
-            Text("Hello, worldsw!")
-=======
-            Text("Test Commit")
->>>>>>> 2f68f42 (Test commit into github)
->>>>>>> 2eedda28d3f45e4053bf49cb76985145f6cf4312
         }
         .padding()
     }
