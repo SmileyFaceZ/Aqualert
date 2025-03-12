2f68f42d1d76fb9627a408e7deb5561fa658d3ac
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2025-03-12 16:38:24 +0700

    Test commit into github

---
+++ Aqualert/AqualertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AqualertApp.swift
+//  Aqualert
+//
+//  Created by Kantaphat Phaphui on 12/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct AqualertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
