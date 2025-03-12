25bb457f8f728fb567ca40d7a8a1beb767c9df06
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2025-03-12 16:38:24 +0700

    Initial Commit

---
+++ Aqualert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aqualert
+//
+//  Created by Kantaphat Phaphui on 12/3/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worldsw!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
