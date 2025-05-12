821286172fbc92cdbcdd8d2ef90118c81856130c
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2025-05-12 15:52:41 +0700

    Change API_URL for onrender deploy

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,3 +1,1 @@
-// export const API_URL = "https://aqualert.onrender.com/api";
-// export const API_URL = "http://localhost:3000/api";
-export const API_URL = "http://192.168.1.37:3000/api";
+export const API_URL = "https://aqualert.onrender.com/api";
